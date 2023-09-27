0135c1cab46bde2028c78a7f11c79f6c72ee84b1
DouglasLindahl <douglas.lindahl@gmail.com>
2023-09-27 13:29:22 +0200

    index page images

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import Link from "next/link";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/footer";
@@ -7,7 +6,26 @@ import styled from "styled-components";
 
 const Main = styled.div`
   background-color: white;
-  height: 1000px;
+  .img{
+    background-repeat: no-repeat;
+    background-position: center;
+    background-color: black;
+    padding-bottom: 64px;
+  }
+  .heroOne{
+    background-image: url("images/heroOne.png");
+    height: 600px;
+    display: flex;
+    align-items: center;
+  }
+  .heroTwo{
+    background-image: url("images/heroTwo.png");
+    height: 440px;
+  }
+  .heroThree{
+    background-image: url("images/heroThree.png");
+    height: 440px;
+  }
 `;
 const Section = styled.section`
   color: white;
@@ -32,6 +50,7 @@ const RegisterBtn = styled(Link)`
   background-color: white;
   color: black;
   padding: 12px 16px;
+  width: 92%;
 `;
 
 export default function Home() {
@@ -39,16 +58,14 @@ export default function Home() {
     <Main>
       <Navbar></Navbar>
 
-      <Section>
+      <Section className="heroOne img">
         <InfoText>Ta kontroll över dina prenumerationer med Submind</InfoText>
-        <div>
           <RegisterBtn href="/register">Kom igång! - Det är gratis</RegisterBtn>
-        </div>
       </Section>
-      <Section>
+      <Section className="heroTwo img">
         <InfoText>Bli påmmind om när dina gratisperioder löper ut!</InfoText>
       </Section>
-      <Section>
+      <Section className="heroThree img">
         <InfoText>Få en överblick av din totala kostnad</InfoText>
       </Section>
       <Footer></Footer>
