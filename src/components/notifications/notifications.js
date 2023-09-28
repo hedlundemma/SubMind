faa1115d0c2990db90c9300ae1b76d1f308877fb
hedlundemma <emmahedlund93@gmail.com>
2023-09-28 13:34:12 +0200

    notification

--- src/components/notifications/notifications.js
+++ src/components/notifications/notifications.js
@@ -1,61 +1,61 @@
-'use client'
+"use client";
 
-import { CONFIG } from '@/config'
+import { CONFIG } from "@/config";
 
 const notificationsSupported = () =>
-  'Notification' in window &&
-  'serviceWorker' in navigator &&
-  'PushManager' in window
+  "Notification" in window &&
+  "serviceWorker" in navigator &&
+  "PushManager" in window;
 
 export default function Notifications() {
-    subscribe();
+  subscribe();
   if (!notificationsSupported()) {
-    return <h3>Please install the PWA first!</h3>
+    return <h3>Please install the PWA first!</h3>;
   }
 }
 
 export const unregisterServiceWorkers = async () => {
-  const registrations = await navigator.serviceWorker.getRegistrations()
-  await Promise.all(registrations.map((r) => r.unregister()))
-}
+  const registrations = await navigator.serviceWorker.getRegistrations();
+  await Promise.all(registrations.map((r) => r.unregister()));
+};
 
 const registerServiceWorker = async () => {
-  return navigator.serviceWorker.register('/service.js')
-}
-
+  return navigator.serviceWorker.register("/service.js");
+};
+//
 const subscribe = async () => {
-  await unregisterServiceWorkers()
+  await unregisterServiceWorkers();
 
-  const swRegistration = await registerServiceWorker()
-  await window?.Notification.requestPermission()
+  const swRegistration = await registerServiceWorker();
+  await window?.Notification.requestPermission();
 
   try {
     const options = {
       applicationServerKey: CONFIG.PUBLIC_KEY,
       userVisibleOnly: true,
-    }
-    const subscription = await swRegistration.pushManager.subscribe(options)
+    };
+    const subscription = await swRegistration.pushManager.subscribe(options);
 
-    await saveSubscription(subscription)
-    
+    await saveSubscription(subscription);
+
     //save subsctipion endpoint to user in supabase
 
-    console.log({ subscription })
+    console.log({ subscription });
   } catch (err) {
-    console.error('Error', err)
+    console.error("Error", err);
   }
-}
+};
 
 const saveSubscription = async (subscription) => {
-  const ORIGIN = window.location.origin
-  const BACKEND_URL = `${ORIGIN}/api/push`
+  const ORIGIN = window.location.origin;
+  const BACKEND_URL = `${ORIGIN}/api/push`;
 
   const response = await fetch(BACKEND_URL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(subscription),
-  })
-  return response.json()
-}
+  });
+  return response.json();
+};
